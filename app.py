bd24ec3c95ddc4f7140c2f9feb0751e089768913
Nermin Kılıçarslan <kilicarslannermin903@gmail.com>
2026-06-05 22:51:16 +0300

    Open app in system browser on launch

--- app.py
+++ app.py
@@ -283,4 +283,4 @@ with gr.Blocks(css=CSS, title="Lupe Fiasco GPT") as demo:
     """)
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, show_error=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860, show_error=True, inbrowser=True)
